95c43d4a6c424415c264b677ce6c3b37f2a09682
Mike T <fierydrake@no.spam>
2012-03-13 00:57:21 +0000

    LocaleMenu: left/right arrow keys change column

--- src/com/mojang/mojam/gui/menus/LocaleMenu.java
+++ src/com/mojang/mojam/gui/menus/LocaleMenu.java
@@ -3,11 +3,13 @@ package com.mojang.mojam.gui.menus;
 import java.awt.event.KeyEvent;
 
 import com.mojang.mojam.gameinput.LocalGameInput;
+import com.mojang.mojam.gameinput.PhysicalInputs;
 import com.mojang.mojam.gameview.GameView;
 import com.mojang.mojam.gui.Font;
 import com.mojang.mojam.gui.components.BackButton;
 import com.mojang.mojam.gui.components.Button;
 import com.mojang.mojam.gui.components.ClickableComponent;
+import com.mojang.mojam.math.Mth;
 import com.mojang.mojam.resources.Constants;
 import com.mojang.mojam.resources.Texts;
 import com.mojang.mojam.screen.Art;
@@ -15,11 +17,12 @@ import com.mojang.mojam.screen.Screen;
 
 public class LocaleMenu extends GuiMenu {
 	public static final String[] LOCALES = { 
-		"en", "de", "es", "fr", "ind", "sv", "it", "nl", "pt_br", "ru", "sl", "af" 
+		"en", "de", "es", "fr", "ind", "sv", "it", "nl", "pt_br", "ru", "sl", "af"
 	};
 	private final Button[] LOCALE_BUTTONS = new Button[LOCALES.length]; 
 
 	private int textY;
+	private int numRows;
 	private Button back;
 
 	public LocaleMenu() {
@@ -32,14 +35,15 @@ public class LocaleMenu extends GuiMenu {
 		int yOffset = (GameView.HEIGHT - (7 * offset + 20 + (offset * 2))) / 2;
 		textY = yOffset;
 		yOffset += offset;
+		numRows = (LOCALE_BUTTONS.length + 1) / 2; /* odd numbers will round up */
 		int TopYOffset = yOffset;
 		
-		for (int i=0; i<LOCALE_BUTTONS.length/2; i++) { 
+		for (int i=0; i<numRows; i++) { 
 			LOCALE_BUTTONS[i] = new Button(Constants.getString("options.locale_" + LOCALES[i]), left_xOffset, (yOffset += offset), false);
 			addButton(LOCALE_BUTTONS[i]);
 		}
 		yOffset = TopYOffset;
-		for (int i=LOCALE_BUTTONS.length/2+1; i<LOCALE_BUTTONS.length; i++) { 
+		for (int i=numRows; i<LOCALE_BUTTONS.length; i++) { 
 			LOCALE_BUTTONS[i] = new Button(Constants.getString("options.locale_" + LOCALES[i]), right_xOffset, (yOffset += offset), false);
 			addButton(LOCALE_BUTTONS[i]);
 		}
@@ -59,8 +63,19 @@ public class LocaleMenu extends GuiMenu {
 
 	@Override
 	public void tick(LocalGameInput input) {
-		if (input.getCurrentPhysicalState().wasKeyPressedConsume(KeyEvent.VK_ESCAPE)) {
+		PhysicalInputs inputs = input.getCurrentPhysicalState();
+		if (inputs.wasKeyPressedConsume(KeyEvent.VK_ESCAPE)) {
 			back.postClick();
+		}
+		if (focusedItem + 1 <= numRows) {
+			if (inputs.wasKeyPressedConsume(KeyEvent.VK_RIGHT, KeyEvent.VK_D)) {
+				focusedItem = Mth.clamp(focusedItem + numRows, 0, LOCALE_BUTTONS.length - 1);
+			}
+		}
+		if (focusedItem + 1 > numRows && focusedItem < LOCALE_BUTTONS.length) {
+			if (inputs.wasKeyPressedConsume(KeyEvent.VK_LEFT, KeyEvent.VK_A)) {
+				focusedItem -= numRows;
+			}
 		}
 		super.tick(input);
 	}
